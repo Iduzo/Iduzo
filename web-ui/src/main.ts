2a5ac3846d43221bcd94cc63517102c0af498695
VivienLambert <lambertvivien@yahoo.fr>
2022-09-10 20:58:59 +0200

    add Element plus into main.ts

--- web-ui/src/main.ts
+++ web-ui/src/main.ts
@@ -3,5 +3,12 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import fr from "element-plus/es/locale/lang/fr";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(ElementPlus, { locale: fr })
+  .mount("#app");
