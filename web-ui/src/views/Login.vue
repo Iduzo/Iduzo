524d8294ea07c014c41480464db0726099ce7096
VivienLambert <lambertvivien@yahoo.fr>
2022-09-11 23:15:54 +0200

    add first view

--- web-ui/src/views/Login.vue
+++ web-ui/src/views/Login.vue
@@ -1,17 +1,6 @@
 <template>
   <div  style="height: 100%; display: flex; justify-content: space-around; flex-direction: column;">
-    <multi-lines :lines="lines"/>
-    <div>
-      <h1>A vous de jouer !</h1>
-      <div>
-        <el-button round type="primary" @click="()=>router.push({name:'login'})">
-          <h1>GO</h1> 
-          <el-icon>
-            <ArrowRightBold/>
-          </el-icon>
-        </el-button>
-      </div>
-    </div>
+   login
   </div>
 </template>
 
