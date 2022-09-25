26d3db77d0b7bc0ea28196bc3e5bc96930bdf84c
VivienLambert <lambertvivien@yahoo.fr>
2022-09-25 23:07:50 +0200

    test ifc loader

--- web-ui/src/components/tool/Scene.vue
+++ web-ui/src/components/tool/Scene.vue
@@ -1,11 +1,13 @@
 <template>
   <div style="height:100%; width:100%" :ref="containerId">
-
   </div>
 </template>
 
 <script>
 import * as THREE from "three";
+// import { IFCLoader } from "web-ifc-three/IFCLoader";
+import { IfcViewerAPI } from "web-ifc-viewer";
+//import { acceleratedRaycast, computeBoundsTree, disposeBoundsTree } from "three-mesh-bvh";
 
 export default {
   name: 'ToolScene',
@@ -18,32 +20,70 @@ export default {
 
   methods: {
     renderScene() {
-      this.renderer.render(this.scene, this.camera);
+      // this.renderer.render(this.scene, this.camera);
     },
-    addRoom() {
-      const geometry = new THREE.BoxGeometry(20, 20, 20);
-      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-      const cube = new THREE.Mesh( geometry, material );
-      this.scene.add( cube );
+    async loadIFC() {
+      this.ifcModels = [];
+      // this.ifcLoader = new IFCLoader();
+      this.viewer.IFC.setWasmPath("../IFCLoader");
+      this.viewer.IFC.loadIfcUrl("../IFCLoader/house.ifc")
+      //   , (ifcModel) => {
+      //   this.ifcModels.push(ifcModel);
+      // });
+      // console.log(this.ifcModels)
+      // this.raycaster = new THREE.Raycaster();
+      // this.raycaster.firstHitOnly = true;
+      // this.mouse = new THREE.Vector2();
     },
+    // cast(event) {
+    //   // Computes the position of the mouse on the screen
+    //   const bounds = this.renderer.domElement.getBoundingClientRect();
+
+    //   const x1 = event.clientX - bounds.left;
+    //   const x2 = bounds.right - bounds.left;
+    //   this.mouse.x = (x1 / x2) * 2 - 1;
+
+    //   const y1 = event.clientY - bounds.top;
+    //   const y2 = bounds.bottom - bounds.top;
+    //   this.mouse.y = -(y1 / y2) * 2 + 1;
+
+    //   // Places it on the camera pointing to the mouse
+    //   this.raycaster.setFromCamera(this.mouse, this.camera);
+
+    //   // Casts a ray
+    //   return this.raycaster.intersectObjects(this.ifcModels);
+    // },
     init() {
-      this.renderer = new THREE.WebGLRenderer();
       const container = this.$refs[this.containerId]
-      this.renderer.setSize(container.clientWidth, container.clientHeight);
-      console.log(container.clientWidth)
+      this.viewer = new IfcViewerAPI({
+        container,
+        backgroundColor: new THREE.Color(0xffffff),
+      });
+      this.viewer.axes.setAxes();
+      this.viewer.grid.setGrid();
+      window.onkeydown = (event) => {
+        if (event.code === "KeyP") {
+          this.viewer.clipper.createPlane();
+        } else if (event.code === "KeyO") {
+          this.viewer.clipper.deletePlane();
+        }
+      };
+
+      // this.renderer = new THREE.WebGLRenderer();
+      // this.renderer.setSize(container.clientWidth, container.clientHeight);
       
-      container.appendChild( this.renderer.domElement );
+      // container.appendChild(this.renderer.domElement);
 
-      this.camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
-      this.camera.position.set( 0, 0, 100 );
-      this.camera.lookAt( 0, 0, 0 );
+      // this.camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
+      // this.camera.position.set( -200, -200, 100 );
+      // this.camera.lookAt( 0, 0, 0 );
 
-      this.scene = new THREE.Scene();
+      // this.scene = new THREE.Scene();
     }
   },
-  mounted() {
+  async mounted() {
     this.init()
-    this.addRoom()
+    await this.loadIFC()
     this.renderScene()
   },
 }
